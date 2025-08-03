fbf6871710f8412604538b30c13639dbd4de6e1a
Abhi Patel <abhipatel@Mac.lan>
2025-08-03 15:41:28 +0530

    Adding new tab button

--- web.py
+++ web.py
@@ -23,13 +23,21 @@ class browser(QMainWindow):
         self.forwardButton = QPushButton("Forth")
         self.forwardButton.clicked.connect(self.forthPageMethod)
         self.forwardButton.setFixedSize(60, 30)
-        
+
+        self.newTabButton = QPushButton("Add")
+        self.newTabButton.setFixedSize(60, 30)
+        self.newTabButton.clicked.connect(self.newTabMethod)
 
         layout = QGridLayout()
-        layout.addWidget(self.inputBar, 0, 2)
+        layout.setContentsMargins(10, 10, 10, 10)
+        layout.setSpacing(10)
+
+        layout.addWidget(self.inputBar, 0, 3)
         layout.addWidget(self.previousButton, 0, 0)
         layout.addWidget(self.forwardButton, 0, 1)
-        layout.addWidget(self.browser, 1, 0, 2, 0)
+        layout.addWidget(self.newTabButton, 0, 2)
+        
+        layout.addWidget(self.browser, 3, 0, 1, 4)
 
         container = QWidget()
         container.setLayout(layout)
@@ -49,6 +57,9 @@ class browser(QMainWindow):
     def forthPageMethod(self):
         if self.browser.history().canGoForward():
             self.browser.forward()
+        
+    def newTabMethod(self):
+        pass
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
