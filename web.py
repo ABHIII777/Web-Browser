94758cc13884c875c9042175a89849132720eba4
Abhi Patel <abhipatel@Mac.lan>
2025-08-03 14:33:35 +0530

    removal of comments

--- web.py
+++ web.py
@@ -7,12 +7,8 @@ class browser(QMainWindow):
     def __init__(self):
         super().__init__()
         
-        # Setting up main window
-        
         self.setWindowTitle("BROWSER")
         self.setGeometry(100, 100, 1200, 800)
-
-        #  Web Engine and URL bar
 
         self.browser = QWebEngineView()
         self.browser.setUrl(QUrl("http://www.google.com"))
@@ -20,26 +16,17 @@ class browser(QMainWindow):
         self.inputBar = QLineEdit()
         self.inputBar.returnPressed.connect(self.navigationMethod)
 
-        # Navigation buttons
-
         self.navigationButton = QPushButton("navigate")
         self.navigationButton.clicked.connect(self.navigationMethod)
-        
-        # Layout setup
         
         layout = QVBoxLayout()
         layout.addWidget(self.inputBar)
         layout.addWidget(self.navigationButton)
         layout.addWidget(self.browser)
 
-        # Central Widget
-        
         container = QWidget()
         container.setLayout(layout)
         self.setCentralWidget(container)
-
-        
-        # Navigation Method
 
     def navigationMethod(self):
         text = self.inputBar.text()
@@ -47,7 +34,6 @@ class browser(QMainWindow):
             text = "http://" + text
 
         self.browser.setUrl(QUrl(text))
-
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
